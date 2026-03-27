e8be5846981d249752407dc34f439da7ac0c3dbb
rajneeshgit <curiousrajneesh2024@gmail.com>
2026-03-27 23:37:29 +0530

    Time: 0 ms (100%), Space: 8.3 MB (12.4%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
- 
-        if (n <= 0) {
-            return false;
+        if (n <= 0) return false;
+
+        string binary = "";
+        int temp = n;
+
+        while (temp > 0) {
+            binary = char((temp % 2) + '0') + binary;
+            temp = temp / 2;
         }
-        
-        if ((n & (n - 1)) == 0) {
-            return true;
+
+        int count = 0;
+        for (int i = 0; i < binary.length(); i++) {
+            if (binary[i] == '1') {
+                count++;
+            }
         }
-        else {
-            return false;
-        }
+
+        return count == 1;
     }
 };
